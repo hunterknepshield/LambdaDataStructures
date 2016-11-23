c5c2ea8dd4de57aba48f6ad42279b6d7f336b32b
Hunter Knepshield <theragingfatcat@gmail.com>
2016-11-23 16:04:16 -0500

    Initial Commit

---
+++ LambdaDataStructures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LambdaDataStructures
+//
+//  Created by Hunter Knepshield on 11/23/16.
+//  Copyright © 2016 Hunter Knepshield. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
